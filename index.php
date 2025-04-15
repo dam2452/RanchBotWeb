916d5316060de132b04868449a8a566e631a9a8b
dam2452 <81230036+dam2452@users.noreply.github.com>
2025-04-15 11:24:07 +0200

    .

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="color-scheme" content="only light">
     <title>RanchBot</title>
     <link rel="stylesheet" href="style.css" />
+
 </head>
 <body>
 <header>
@@ -32,8 +36,8 @@
         </div>
 
         <div class="actions">
-            <button onclick="createAccount()">Create account ?</button>
-            <button onclick="forgotPassword()">Forgot password ?</button>
+            <button >Create account ?</button>
+            <button >Forgot password ?</button>
         </div>
     </section>
 </main>
--- login.php
+++ login.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $login = trim($_POST['login'] ?? '');
     $password = trim($_POST['password'] ?? '');
 
-    if ($login === 'admin' && $password === 'ranch123') {
+    if ($login === 'admin' && $password === 'admin') {
         echo "Welcome, $login!";
     } else {
         echo "Invalid login or password.";
